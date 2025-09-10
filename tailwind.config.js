852f540188c24225745c27898f7860d283b3dc7f
Peterson Amblessed <amblessedpetercns@gmail.com>
2025-09-10 12:36:06 +0100

    Initial commit: Expo + NativeWind setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./App.tsx", "./components/**/*.{js,jsx,ts,tsx}"],
+  content: ["./App.tsx", "./components/**/*.{js,jsx,ts,tsx}", "./app/**/*.{js,ts,jsx,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
     extend: {},
