541f431fafc9e523b3bf2d4212724e49a83c5041
Peterson Amblesed <petersonamblessed@gmail.com>
2025-09-23 11:33:16 +0100

    `Added global CSS import and modified RootLayout to include a hidden screen with name "(tabs)"`

---
+++ app/(tabs)/Profle.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const Profle = () => {
+  return (
+    <View>
+      <Text>Profle</Text>
+    </View>
+  )
+}
+
+export default Profle
---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,45 @@
+import { Tabs } from 'expo-router'
+
+const _layout = () => {
+  return (
+    <Tabs>
+      <Tabs.Screen 
+        name='index'
+        options={{
+          title: 'Home',
+          headerShown: false,
+        }}
+      />
+      <Tabs.Screen 
+        name='Property'
+        options={{
+          title: 'Property',
+          headerShown: false
+        }}
+      />
+      <Tabs.Screen 
+        name='Search'
+        options={{
+          title: 'Search',
+          headerShown: false
+        }}
+      />
+      <Tabs.Screen 
+        name='Favourite'
+        options={{
+          title: 'Favourite',
+          headerShown: false
+        }}
+      />
+      <Tabs.Screen 
+        name='Profile'
+        options={{
+          title: 'Profile',
+          headerShown: false
+        }}
+      />
+    </Tabs>
+  )
+}
+
+export default _layout
